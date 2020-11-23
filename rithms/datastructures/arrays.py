10d600fce963f36084b5e4da302f2d808cdbf884
Jorge Luis Rodríguez González <egroJ97@gmail.com>
2020-11-22 20:09:57 -0400

    Added support for DynamicArrays and improved on static arrays design

--- rithms/datastructures/arrays.py
+++ rithms/datastructures/arrays.py
@@ -4,19 +4,80 @@ class Array:
             raise ValueError('Size of the array must be greater than zero (0)')
 
         if data is None:
-            data = [0 for _ in range(size)]
+            data = [None for _ in range(size)]
 
-        self.size = size if size > 0 else len(data)
+        self._size = size if size > 0 else len(data)
         self._data = data
 
     def __getitem__(self, key: int):
-        if self.size <= key:
+        if self._size < key:
             raise TypeError('key must be smaller than array size')
 
         return self._data[key]
 
     def __setitem__(self, key: int, value):
-        if self.size <= key:
+        if self._size < key:
             raise TypeError('key must be smaller than array size')
 
         self._data[key] = value
+
+    def size(self):
+        return self._size
+
+    def first(self):
+        return self.__getitem__(0)
+
+    def last(self):
+        return self.__getitem__(self.size() - 1)
+
+
+class DynamicArray:
+    def __init__(self, size: int = 0, data=None):
+        self._data = Array(size, data)
+        self._capacity = self._data.size()
+        index = 0
+        while index < self._data.size() and self._data[index]:
+            index += 1
+        self._last_index = index-1
+
+    def append(self, value):
+        if self._last_index+1 == self._capacity:
+            temp = Array(size=self._data.size() * 2)
+            for i in range(self._capacity):
+                temp[i] = self.__getitem__(i)
+
+            self._data = temp
+            self._capacity = self._data.size()
+
+        self._data[self._last_index+1] = value
+        self._last_index += 1
+
+    def pop(self):
+        if (self._last_index <= (self._data.size() // 4)) and (self._data.size() > 2):
+            resize = self._data.size() // 2
+            temp = Array(size=resize)
+            for i in range(resize):
+                temp[i] = self.__getitem__(i)
+            self._data = temp
+
+        self._data[self._last_index] = None
+        self._last_index -= 1
+        self._capacity = self._data.size()
+
+    def __getitem__(self, key: int):
+        if self._capacity <= key:
+            raise TypeError('key must be smaller than array size')
+
+        return self._data[key]
+
+    def __setitem__(self, key: int, value):
+        if self._capacity <= key:
+            raise TypeError('key must be smaller than array size')
+
+        self._data[key] = value
+
+    def size(self):
+        return self._last_index+1
+
+    def last(self):
+        return self._data[self._last_index]
--- tests/datastructures_tests/test_array.py
+++ tests/datastructures_tests/test_array.py
@@ -46,5 +46,67 @@ class ArrayTestSuite(unittest.TestCase):
             self.assertTrue(True)
 
 
+class DynamicArrayTestSuite(unittest.TestCase):
+    def test_dynamic_array_creation(self):
+        self.assertTrue(arrays.DynamicArray(10))
+
+    def test_dynamic_array_getting(self):
+        arr = arrays.DynamicArray(data=[1, 2, 3, 4])
+        self.assertEqual(arr[0], 1)
+        self.assertEqual(arr[1], 2)
+        self.assertEqual(arr[2], 3)
+        self.assertEqual(arr[3], 4)
+
+    def test_dynamic_array_setting(self):
+        arr = arrays.DynamicArray(4)
+
+        arr[0] = 1
+        arr[1] = 2
+        arr[2] = 3
+        arr[3] = 4
+
+        self.assertEqual(arr[0], 1)
+        self.assertEqual(arr[1], 2)
+        self.assertEqual(arr[2], 3)
+        self.assertEqual(arr[3], 4)
+
+    def test_failure_on_null_dynamic_array(self):
+        try:
+            arrays.DynamicArray(data=[])
+        except ValueError:
+            self.assertTrue(True)
+
+    def test_failure_on_zero_dynamic_array(self):
+        try:
+            arrays.DynamicArray(0)
+        except ValueError:
+            self.assertTrue(True)
+
+    def test_failure_on_negative_sized_dynamic_array(self):
+        try:
+            arrays.DynamicArray(-10)
+        except ValueError:
+            self.assertTrue(True)
+
+    def test_append_dynamic_array(self):
+        arr = arrays.DynamicArray(data=[1, 2, 3, 4, 5, 6])
+        for i in range(100):
+            arr.append(i)
+
+        self.assertEqual(arr.last(), 99)
+        self.assertEqual(arr.size(), 106)
+
+    def test_pop_dynamic_array(self):
+        arr = arrays.DynamicArray(data=[1, 2, 3, 4, 5, 6])
+        for i in range(6):
+            arr.pop()
+
+        try:
+            arr.last()
+        except ValueError:
+            self.assertTrue(True)
+        self.assertEqual(arr.size(), 0)
+
+
 if __name__ == '__main__':
     unittest.main()
